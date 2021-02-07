6c4275de241883b2a52d0f14413514eb3ce6758a
Jason Dempsey <jasondemps1@gmail.com>
2021-02-06 22:08:19 -0500

    Implemented querying A, NS, CNAME. Might be a couple of bugs but works.

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,15 +1,52 @@
+const { DefaultAzureCredential } = require("@azure/identity");
+const { DnsManagementClient } = require("@azure/arm-dns");
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    //const name = (req.query.name || (req.body && req.body.name));
-    //const responseMessage = name
-        //? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        //: "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    let outputText = 'Hi!     -The API';
+
+    // when deployed to an azure host the default azure credential will authenticate the specified user assigned managed identity
+    const credential = new DefaultAzureCredential();
+    const dnsClient = new DnsManagementClient(credential, process.env["AZURE_SUBSCRIPTION_ID"]);
+
+    const recordTypes = ["A", "NS", "CNAME"];
+
+    let dnsMappings = [];
+    try {
+        for (const recordType of recordTypes) {
+            const results = await dnsClient.recordSets.listByType(process.env["AZURE_RESOURCE_GROUP"], process.env["AZURE_DNS_ZONE"], recordType)
+            .then(result => {
+                context.log(result);
+                return result;
+            })
+            .catch(err => {
+                context.error(err);
+                throw err;
+            });
+
+            for (const result of results) {
+                dnsMappings.push({
+                    name: result.name,
+                    fqdn: result.fqdn,
+                });
+            }
+        }
+    } catch (err) {
+        outputText += err;
+    }
+
+    if (dnsMappings.length > 0) {
+        outputText = '';
+
+        for (const dnsMapping of dnsMappings) {
+            outputText += ` | <a href="${dnsMapping.fqdn}" target="_blank">${dnsMapping.name}</a>`;
+        }
+    }
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
         body: {
-            text: "Hi, from the API!" //responseMessage
+            text: outputText
         }
     };
 }
